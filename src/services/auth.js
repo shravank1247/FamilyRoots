2ae964de27fe3c2ca170220bc33c724a29360124
itsmyfamilytree <itsmyfamilytree@gmail.com>
2025-12-17 19:20:17 +0530

    changed path in auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -20,9 +20,10 @@ export async function signInWithGoogle() {
     await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: window.location.origin + '/dashboard', 
+            redirectTo: window.location.origin , 
         }
     });
+    return { data, error };
 }
 
 export async function checkAuth() {
