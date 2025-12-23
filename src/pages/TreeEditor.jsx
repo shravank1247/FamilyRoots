e1f59e7f39f114d5e9abf9662086de6a222e6dcf
itsmyfamilytree <itsmyfamilytree@gmail.com>
2025-12-23 18:04:44 +0530

    refining the code to adhearpermission logic 13

--- src/pages/TreeEditor.jsx
+++ src/pages/TreeEditor.jsx
@@ -550,8 +550,11 @@ const canEditProperties = userRole === 'edit' || userRole === 'full';
 const canAddOrDelete = userRole === 'full';
 
 // 3. THE "SAFETY GATE" (Only return JSX here, after all hooks)
-if (!session || !session.user) {
-    return <div className="loading-screen">Checking Permissions...</div>; //
+if (!session) {
+    return <div className="loading-screen">Checking Permissions session 1...</div>; //
+}
+else if (!session.user) {
+    return <div className="loading-screen">Checking Permissions session user...</div>; //
 }
 
     return (
